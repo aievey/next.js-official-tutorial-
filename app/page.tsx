72846de14214f05d0b9fb6e1491016c06c88e9f0
aievey <amitabh283verma@gmail.com>
2026-01-03 10:48:32 +0530

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import styles from "@/app/ui/home.module.css";
-import { lusitana } from "@/app/ui/font.ts";
+import { lusitana } from "@/app/ui/font";
 import Image from "next/image";
 export default function Page() {
   return (
@@ -34,6 +34,7 @@ export default function Page() {
             src="/hero-desktop.png"
             width={1000}
             height={760}
+            priority
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
